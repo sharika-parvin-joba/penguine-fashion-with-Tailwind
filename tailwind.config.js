a62f148f45e91c05a0f55074d2acb2eac9f00cba
unknown <sharikajoba@gmail.com>
2022-07-30 23:50:41 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */ 
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+
+
+
+ // we need this file for our every tailwind project
+ //And we can find this from tailwind doc..file name will be tailwind.config.js..we just need to copy the file and paste it here with the same name
